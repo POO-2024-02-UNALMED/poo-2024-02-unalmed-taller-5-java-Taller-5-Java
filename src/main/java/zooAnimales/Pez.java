817b643970d4b5c25b9ee12a556ec14694152122
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 09:11:16 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,4 +1,21 @@
 package zooAnimales;
 public class Pez extends Animal{
-  
+  private Pez[] listado;
+  public Integer ranas;
+  public Integer salamandras;
+  private String colorPiel;
+  private Boolean venenoso;
+
+  public void cantidadAnfibios(){
+    
+  }
+  public void movimiento(){
+    
+  }
+  public void crearRana(){
+    
+  }
+  public void crearSalamandra(){
+    
+  }
 }
