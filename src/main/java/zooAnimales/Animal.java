4c40432d05b3962d1fb796d625adbdc5084a77de
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 10:12:15 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -10,6 +10,7 @@ public class Animal {
   private Zona[] zona = new Zona[1];
 
   public Animal() {
+     totalAnimales++;
     }
 
   public Animal(Integer totalAnimales, String nombre, String habitat, String genero, Zona[] zona) {
@@ -18,15 +19,50 @@ public class Animal {
         this.habitat = habitat;
         this.genero = genero;
         this.zona = zona;
+        totalAnimales++;
     }
   
-  public void movimiento(){
-    
+  public String movimiento(){
+    return "desplazarse";
   }
-  public void totalPorTipo(){
-    
-  }
+public String totalPorTipo(Animal[] animales) {
+    int mamiferos = 0;
+    int aves = 0;
+    int reptiles = 0;
+    int peces = 0;
+    int anfibios = 0;
+
+    for (Animal animal : animales) {
+        if (animal != null) {
+            if (animal instanceof Mamifero) {
+                mamiferos++;
+            } else if (animal instanceof Ave) {
+                aves++;
+            } else if (animal instanceof Reptil) {
+                reptiles++;
+            } else if (animal instanceof Pez) {
+                peces++;
+            } else if (animal instanceof Anfibio) {
+                anfibios++;
+            }
+        }
+    }
+    return "Mamíferos: " + mamiferos + "\n" +
+           "Aves: " + aves + "\n" +
+           "Reptiles: " + reptiles + "\n" +
+           "Peces: " + peces + "\n" +
+           "Anfibios: " + anfibios;
+}
+
   @Override
   public String toString() {
-    return "a";
+        String resultado = "Mi nombre es " + nombre +
+                           ", tengo una edad de desconocida, habito en " + habitat +
+                           " y mi género es " + genero;
+        if (zona != null && zona[0] != null) {
+            resultado += ", la zona en la que me ubico es " + zona[0].getNombre() +
+                         ", en el " + (zona[0].getZoo()[0] != null ? zona[0].getZoo()[0].getNombre() : "zoo desconocido");
+        }
+        return resultado;
+    }
 }
