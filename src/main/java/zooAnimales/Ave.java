6488f8d278d025f473e26aed14395ecaf974f140
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 09:01:24 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,4 +1,22 @@
 package zooAnimales;
 public class Ave extends Animal{
   
+  private Ave[] listado;
+  public Integer halcones;
+  public Integer aguilas;
+  private String colorPlumas;
+
+  public void cantidadAves(){
+    
+  }
+  public void movimiento(){
+    
+  }
+
+  public void crearHalcon(){
+    
+  }
+  public void crearAguila(){
+    
+  }
 }
