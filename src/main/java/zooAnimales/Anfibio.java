921d55955b5bad9c30f239841b131d9040ced72d
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 09:45:18 -0500

    Update Anfibio.java

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,4 +1,33 @@
 package zooAnimales;
 public class Anfibio extends Animal{
-  
+  private Anfibio[] listado;
+  public Integer ranas;
+  public Integer salamandras;
+  private String colorPiel;
+  private Boolean venenoso;
+
+  public Anfibio() {
+    }
+
+  public Anfibio(Anfibio[] listado, Integer ranas, Integer salamandras, String colorPiel, Boolean venenoso) {
+        this.listado = listado;
+        this.ranas = ranas;
+        this.salamandras = salamandras;
+        this.colorPiel = colorPiel;
+        this.venenoso = venenoso;
+    }
+
+
+  public void cantidadAnfibios(){
+    
+  }
+  public void movimiento(){
+    
+  }
+  public void crearRana(){
+    
+  }
+  public void crearSalamandra(){
+    
+  }
 }
