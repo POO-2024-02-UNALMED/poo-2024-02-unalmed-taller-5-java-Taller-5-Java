fda634677cbce19a8c851a57c062118d350fd49f
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 10:43:42 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,32 +1,77 @@
 package zooAnimales;
-public class Pez extends Animal{
-  private Pez[] listado;
-  public Integer salmones;
-  public Integer bacalaos;
-  private String colorEscamas;
-  private Integer cantidadAletas;
 
-  public Pez() {
+public class Pez extends Animal {
+  
+    private Pez[] listado = new Pez[0];
+    public int salmones = 0;
+    public int bacalaos = 0;
+    private String colorEscamas;
+    private int cantidadAletas;
+  
+    public Pez() {
+        agregarAlListado(this);
     }
-  public Pez(Pez[] listado, Integer salmones, Integer bacalaos, String colorEscamas, Integer cantidadAletas) {
-        this.listado = listado;
-        this.salmones = salmones;
-        this.bacalaos = bacalaos;
+
+    public Pez(String nombre, String habitat, String genero, String colorEscamas, int cantidadAletas) {
+        super(nombre, habitat, genero, null);
         this.colorEscamas = colorEscamas;
         this.cantidadAletas = cantidadAletas;
+        agregarAlListado(this);
     }
 
-  
-  public void cantidadPeces(){
-    
-  }
-  public String movimiento(){
-    return "nadar";
-  }
-  public void crearSalmon(){
-    
-  }
-  public void crearBacalao(){
-    
-  }
+    public int cantidadPeces() {
+        return listado.length;
+    }
+
+
+    public Pez crearSalmon(String nombre, String genero) {
+        Pez salmon = new Pez(nombre, "océano", genero, "rojo", 6);
+        salmones++;
+        agregarAlListado(salmon);
+        return salmon;
+    }
+    public Pez crearBacalao(String nombre, String genero) {
+        Pez bacalao = new Pez(nombre, "océano", genero, "gris", 6);
+        bacalaos++;
+        agregarAlListado(bacalao);
+        return bacalao;
+    }
+
+    private void agregarAlListado(Pez pez) {
+        Pez[] nuevoListado = new Pez[listado.length + 1];
+        for (int i = 0; i < listado.length; i++) {
+            nuevoListado[i] = listado[i];
+        }
+        nuevoListado[listado.length] = pez;
+        listado = nuevoListado;
+    }
+
+    public String movimiento() {
+        return "nadar";
+    }
+
+    // Getters y Setters
+    public Pez[] getListado() {
+        return listado;
+    }
+
+    public void setListado(Pez[] listado) {
+        this.listado = listado;
+    }
+
+    public String getColorEscamas() {
+        return colorEscamas;
+    }
+
+    public void setColorEscamas(String colorEscamas) {
+        this.colorEscamas = colorEscamas;
+    }
+
+    public int getCantidadAletas() {
+        return cantidadAletas;
+    }
+
+    public void setCantidadAletas(int cantidadAletas) {
+        this.cantidadAletas = cantidadAletas;
+    }
 }
