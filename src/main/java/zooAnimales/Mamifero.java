0506c3b324c1b1f12223a1c5f17def145c70880e
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 09:35:56 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -7,6 +7,16 @@ public class Mamifero extends Animal {
   private Boolean pelaje;
   private Integer Patas;
 
+  public Mamifero() {
+    }
+
+  public Mamifero(Mamifero[] listado, Integer caballos, Integer leones, Boolean pelaje, Integer patas) {
+        this.listado = listado;
+        this.caballos = caballos;
+        this.leones = leones;
+        this.pelaje = pelaje;
+        this.patas = patas;
+  }
   public void cantidadMamiferos(){
     
   }
