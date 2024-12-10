452b822dde3101cff485d81e9cbbce61fd852915
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 09:06:36 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,4 +1,21 @@
 package zooAnimales;
 public class Reptil extends Animal{
   
+  private Reptil[] listado;
+  public Integer iguanas;
+  public Integer aguilas;
+  private String colorPlumas;
+
+  public void cantidadAves(){
+    
+  }
+  public void movimiento(){
+    
+  } 
+  public void crearHalcon(){
+    
+  }
+  public void crearAguila(){
+    
+  }
 }
