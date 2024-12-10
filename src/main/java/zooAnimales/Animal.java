040b81be7a9e0e6b04b0119749139aa2cd8746f2
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 08:51:23 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -3,5 +3,19 @@ package zooAnimales;
 import gestion.Zona;
 
 public class Animal {
+  private integer totalAnimales;
+  private String nombre;
+  private String habitat;
+  private String genero;
+  private Zona[] zona = new Zona[1];
 
+  public void movimiento(){
+    
+  }
+  public void totalPorTipo(){
+    
+  }
+  @Override
+  public String toString() {
+    return "a";
 }
