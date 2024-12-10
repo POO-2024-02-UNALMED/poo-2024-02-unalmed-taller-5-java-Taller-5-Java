91eaa642b85f34818a55166193c1699dad398321
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 08:41:04 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,4 +1,14 @@
 package gestion;
+import zooAnimales.Animal;
 public class Zona extends Zoologico{
-  
+  private String nombre;
+  private Zoologico[] zoo = new Zoologico[1];
+  private Animal[] animales;
+
+  public void agregarAnimales(){
+    
+  }
+  public void cantidadAnimales(){
+    
+  }
 }
