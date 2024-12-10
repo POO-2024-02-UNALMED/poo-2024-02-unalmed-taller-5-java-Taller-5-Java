f8e6a3a212b8219c756568972fa658f53e852f56
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-12-10 11:11:47 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -34,4 +34,29 @@ public void agregarAnimales(Animal animal) {
 
     return total; // Retorna el total de animales en la zona
   }
+  // Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Zoologico[] getZoo() {
+        return zoo;
+    }
+
+    public void setZoo(Zoologico[] zoo) {
+        this.zoo = zoo;
+    }
+
+    public Animal[] getAnimales() {
+        return animales;
+    }
+
+    public void setAnimales(Animal[] animales) {
+        this.animales = animales;
+    }
 }
